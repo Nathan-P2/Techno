2d2c7bf567e07ca0d6d802fecabcad8c75e54cd8
Nathan Pereira <nathan.pereira0201@gmail.com>
2020-12-12 14:27:54 -0300

    Implementação modal + estilização

--- techno/app.js
+++ techno/app.js
@@ -1,21 +1,38 @@
 const vm = new Vue({
   el: "#app",
   data: {
-    produtos: []
+    produtos: [],
+    produto: "",
   },
   filters: {
     alteraPreco(valor) {
-      console.log(valor)
-      // return valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL"});
+      return valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL"});
     }
   },
   methods: {
     fetchProdutos() {
       fetch("./api/produtos.json")
-      .then(r => r.json())
-      .then(r => {
-        this.produtos = r;
+        .then(r => r.json())
+        .then(r => {
+          this.produtos = r;
+        })
+    },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then(r => r.json())
+        .then(r => {
+          this.produto = r;
+        })
+    },
+    abrirModal (id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
       })
+    },
+    fecharModal({target, currentTarget}) {
+      if(target === currentTarget) this.produto = false;
     }
   },
   created() {
