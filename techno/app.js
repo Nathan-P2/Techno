5316dc8791e5a9d9bab3d89aab1532c088716ba9
Nathan Pereira <nathan.pereira0201@gmail.com>
2020-12-13 18:16:55 -0300

    Implementação Router

--- techno/app.js
+++ techno/app.js
@@ -4,6 +4,8 @@ const vm = new Vue({
       produtos: [],
       produto: false,
       carrinho: [],
+      mensagemAlerta: "Item Adicionado",
+      alertaAtivo: false,
    },
    filters: {
       alteraPreco(valor) {
@@ -50,6 +52,7 @@ const vm = new Vue({
          this.produto.estoque--;
          const {id, nome, preco} = this.produto;
          this.carrinho.push({id, nome, preco});
+         this.alerta(`${nome} foi adicionado ao carrinho!`);
       },
       removerItem(index) {
          this.carrinho.splice(index, 1)
@@ -58,14 +61,33 @@ const vm = new Vue({
          if(window.localStorage.carrinho){
             this.carrinho = JSON.parse(window.localStorage.carrinho);
          } 
+      },
+      alerta(mensagem) {
+         this.mensagemAlerta = mensagem;
+         this.alertaAtivo = true;
+         setTimeout(() =>{
+            this.alertaAtivo = false;
+         }, 1500)
+      },
+      router() {
+         const hash = document.location.hash;
+         if(hash) {
+            this.fetchProduto(hash.replace("#", ""));
+         }
       }
    },
    watch: {
+      produto() {
+         document.title = this.produto.nome || "Techno";
+         const hash = this.produto.id || "";
+         history.pushState(null, null, `#${hash}`)
+      },
       carrinho() {
          window.localStorage.carrinho = JSON.stringify(this.carrinho);
       }
    },
    created() {
+      this.router();
       this.fetchProdutos();
       this.checarLocalStorage();
    }
